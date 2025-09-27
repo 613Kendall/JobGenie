c825751788a4b5b01152dd5043541dd94845ca39
Colin Hoff <hoff.colin@outlook.com>
2025-09-27 12:28:32 -0500

    removed primeng

--- frontend/src/app/app-module.ts
+++ frontend/src/app/app-module.ts
@@ -3,12 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
-import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
-import {providePrimeNG} from 'primeng/config';
-import Aura from '@primeuix/themes/aura'
-import {ButtonModule} from 'primeng/button';
-import {InputNumberModule} from 'primeng/inputnumber';
-import {FileUploadModule} from 'primeng/fileupload';
 import {provideHttpClient} from '@angular/common/http';
 
 @NgModule({
@@ -18,17 +12,8 @@ import {provideHttpClient} from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ButtonModule,
-    InputNumberModule,
-    FileUploadModule
   ],
   providers: [
-    provideAnimationsAsync(),
-    providePrimeNG({
-      theme: {
-        preset: Aura
-      }
-    }),
     provideHttpClient(),
     provideBrowserGlobalErrorListeners(),
   ],
